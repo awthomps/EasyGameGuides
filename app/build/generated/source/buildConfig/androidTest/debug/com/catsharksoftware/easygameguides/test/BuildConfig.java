ba0419c19ebc7a6fa148be9f3431da6626a1b093
Andrei <killosquirrel@gmail.com>
2015-06-10 22:32:22 -0700

    Converting the project to an Android Studio project.

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/catsharksoftware/easygameguides/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.catsharksoftware.easygameguides.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.catsharksoftware.easygameguides.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
